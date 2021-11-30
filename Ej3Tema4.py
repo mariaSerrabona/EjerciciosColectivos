1a44f5af770f3807854ea4b557cc02fc3e4f5aa6
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-30 18:06:20 +0100

    ok

---
+++ Ej3Tema4.py
@@ -0,0 +1,18 @@
+"""Escriba un algoritmo que determine la categoría deportiva de un usuario en función de su edad:
+6 a 7 años: benjamín
+8 a 9 años: alevín
+10 a 11 años: infantil
+12 años y más: cadete
+Escriba el programa Python asociado.
+"""
+edad = int(input("Introduzca su edad en años: "))
+if edad<6:
+    print("No puede jugar en ninguna categoria")
+elif edad == 6 or edad == 7:
+    print("Su categoria es benjamin")
+elif edad == 8 or edad== 9:
+    print("Su categoria es alevin")
+elif edad == 10 or edad== 11:
+    print("Su categoria es infantil")
+elif edad >=12:
+    print("Su categoria es cadete")
