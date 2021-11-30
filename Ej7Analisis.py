5186f76f25ce9282c10c12d12f7a9fcb9e8b2ee7
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-30 21:15:13 +0100

    ok

--- Ej7Analisis.py
+++ Ej7Analisis.py
@@ -1,2 +1,7 @@
 """Invente un experimento para verificar que el operador index para listas es O(1)O(1)
 """
+mi_lista=[83,93,4,6,32,51,20,67,50,29,18,72]
+print(mi_lista[4])
+mi_lista[5]==10
+#Para indexar una lista o reasignar un elemento de esta, el tiempo que se tarda en ejecutar
+# el programa es el mismo siempre, sea el elemento que sea, lo que hace que la complejidad sea O(1).
