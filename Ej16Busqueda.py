924a6047512b07134a03769ae4d48cbd06c1d12f
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-30 18:40:31 +0100

    Enunciado

---
+++ Ej16Busqueda.py
@@ -0,0 +1,3 @@
+"""Implemente el método de mediana de tres para seleccionar un valor pivote como una modificación
+de ordenamientoRapido. Lleve a cabo un experimento para comparar las dos técnicas.
+"""
