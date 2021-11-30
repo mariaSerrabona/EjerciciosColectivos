67d41f5d698c4ef37488e6e4bf782ae28b171c64
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-30 18:03:39 +0100

    ok

---
+++ Ej1Tema4.py
@@ -0,0 +1,10 @@
+"""Escriba, usando comparaciones, un algoritmo que muestre el estado del agua
+(hielo, líquido, vapor) en función de su temperatura.
+"""
+temperatura = 0
+if temperatura<0:
+    print("El agua esta en estado solido")
+elif temperatura>=0 and temperatura<=100:
+    print("El agua esta en estado líquido")
+else:
+    print("El agua esta en estado gaseoso")
